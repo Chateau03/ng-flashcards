12a47a2c64c1bae893b950ebb630b6d9381d5f19
castlixx <raulincastle@gmail.com>
2021-11-21 01:06:05 -0500

    Creando arreglo de flashs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { IFlash } from './flash.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-flashcards';
+  flashs: IFlash[] = [
+    {
+      question: 'Question 1',
+      answer: 'Answer 1',
+      show: false,
+      id: getRandomNumer(),
+    },
+    {
+      question: 'Question 2',
+      answer: 'Answer 2',
+      show: false,
+      id: getRandomNumer(),
+    },
+    {
+      question: 'Question 3',
+      answer: 'Answer 3',
+      show: false,
+      id: getRandomNumer(),
+    },
+  ];
+
+  // trackByFlashId(index, flash) {
+  //   return flash.id;
+  // }
+}
+
+function getRandomNumer() {
+  return Math.floor(Math.random() * 10000);
 }
